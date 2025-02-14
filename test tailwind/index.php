a714bc186a18365dfae937018886f52c3cbed998
BarbierTheo <lebarbier.lh@gmail.com>
2025-02-14 13:54:24 +0100

    test

---
+++ test tailwind/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test tailwind</title>
+    <!-- npx @tailwindcss/cli -i ./style.css -o ./tailwind.css --watch -->
+    <link rel="stylesheet" href="./tailwind.css">
+</head>
+
+<body class="flex flex-col justify-center items-center h-[80vh] w-[100vw] gap-2">
+
+    <h1 class="text-3xl font-bold">est-ce que tailwind marche pareil ?</h1>
+    <h2>oui</h2>
+
+</body>
+
+</html>
